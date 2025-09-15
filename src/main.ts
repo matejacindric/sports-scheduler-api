116bf84d39b71737db43f67f39e5c97b33f2cc55
Mateja Cindrić <mateja.cindric2@gmail.com>
2025-09-15 12:08:01 +0200

    Prepare application bootstrap for improved error handling and server setup:     - add AllExceptionsFilter for global exception handling     - add CORS configuration with credentials and allowed methods

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swagger } from './app.swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { RolesGuard } from './common/guards/roles.guard';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
-async function bootstrap() {
+async function bootstrap() {  
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
@@ -14,6 +15,15 @@ async function bootstrap() {
 
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector), new RolesGuard(reflector));
+
+  app.enableCors({
+    origin: [process.env.FRONTEND_BASE_URL],
+    credentials: true,
+    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
+  });
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
 
   app.enableShutdownHooks();
 
